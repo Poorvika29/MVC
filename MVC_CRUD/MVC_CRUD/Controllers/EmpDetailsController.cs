d25c9eb7c1a5968bcdfb4701a905b5a0debcbff1
Poorvika29 <poorvika29nov@gmail.com>
2018-11-21 01:23:38 +0530

    commit

---
+++ MVC_CRUD/MVC_CRUD/Controllers/EmpDetailsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC_CRUD.Models; 
+
+namespace MVC_CRUD.Controllers
+{
+    public class EmpDetailsController : Controller
+    {
+        private EmpEntities db = new EmpEntities();
+
+        // GET: EmpDetails
+        public ActionResult Index()
+        {
+            var empDetails = db.EmpDetails.Include(e => e.Dept);
+            return View(empDetails.ToList());
+        }
+
+        // GET: EmpDetails/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EmpDetail empDetail = db.EmpDetails.Find(id);
+            if (empDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empDetail);
+        }
+
+        // GET: EmpDetails/Create
+        public ActionResult Create()
+        {
+            ViewBag.DepartmentId = new SelectList(db.Depts, "ID", "Name");
+            return View();
+        }
+
+        // POST: EmpDetails/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmpID,EmpName,Gender,DepartmentId")] EmpDetail empDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EmpDetails.Add(empDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DepartmentId = new SelectList(db.Depts, "ID", "Name", empDetail.DepartmentId);
+            return View(empDetail);
+        }
+
+        // GET: EmpDetails/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EmpDetail empDetail = db.EmpDetails.Find(id);
+            if (empDetail == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DepartmentId = new SelectList(db.Depts, "ID", "Name", empDetail.DepartmentId);
+            return View(empDetail);
+        }
+
+        // POST: EmpDetails/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EmpID,EmpName,Gender,DepartmentId")] EmpDetail empDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(empDetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DepartmentId = new SelectList(db.Depts, "ID", "Name", empDetail.DepartmentId);
+            return View(empDetail);
+        }
+
+        // GET: EmpDetails/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EmpDetail empDetail = db.EmpDetails.Find(id);
+            if (empDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empDetail);
+        }
+
+        // POST: EmpDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EmpDetail empDetail = db.EmpDetails.Find(id);
+            db.EmpDetails.Remove(empDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
